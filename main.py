0816041cc97b76bdc717d23c9bd74d7d07a8e0fa
JustLamer <enea.rusconi@gmail.com>
2025-07-03 10:46:06 +0200

    new path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+
 import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append("/home/tritecc/controller")
 
 import customtkinter as ctk
 from PIL import Image
