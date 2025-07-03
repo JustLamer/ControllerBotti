7b442b96ca8d5a83447143d4d47a0c1b4319e20f
JustLamer <enea.rusconi@gmail.com>
2025-07-03 12:21:36 +0200

    new pin

--- hardware/pinmap.py
+++ hardware/pinmap.py
@@ -2,15 +2,15 @@
 
 BARREL_PINMAP = {
     "Botte 1": {
-        "sensor_pin": 4,    # es. GPIO 4 (cambia secondo necessità)
+        "sensor_pin": 7,    # es. GPIO 4 (cambia secondo necessità)
         "valve_pin": 17,    # es. GPIO 17
     },
     "Botte 2": {
-        "sensor_pin": 5,
+        "sensor_pin": 7,
         "valve_pin": 27,
     },
     "Botte 3": {
-        "sensor_pin": 6,
+        "sensor_pin": 7,
         "valve_pin": 22,
     },
 }
