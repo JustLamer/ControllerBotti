bf1b41ad896fbd51104116ba373488c430f6411c
JustLamer <enea.rusconi@gmail.com>
2025-07-03 10:38:34 +0200

    mm

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import customtkinter as ctk
 from PIL import Image
 from gui.sidebar import Sidebar
