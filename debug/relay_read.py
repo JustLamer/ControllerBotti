55a59f917b17dad187e2aaf4509a6096d928e4ad
JustLamer <enea.rusconi@gmail.com>
2025-07-12 11:48:17 +0200

    Update relay_read.py

--- debug/relay_read.py
+++ debug/relay_read.py
@@ -3,7 +3,6 @@ from pymodbus.client import ModbusSerialClient
 class Relay6CHTest:
     def __init__(self, port='/dev/ttyUSB0', baudrate=9600, slave_id=1):
         self.client = ModbusSerialClient(
-            method='rtu',
             port=port,
             baudrate=baudrate,
             parity='N',
@@ -17,13 +16,7 @@ class Relay6CHTest:
         return self.client.connect()
 
     def read_coils(self, address=0, count=6):
-        """
-        Legge lo stato dei relay (coil) dal modulo
-        :param address: Indirizzo di partenza (di solito 0)
-        :param count: Numero di coil da leggere (per 6 relè, count=6)
-        :return: Lista di booleani con lo stato dei relay
-        """
-        result = self.client.read_coils(address, count, slave=self.slave_id)
+        result = self.client.read_coils(address, count, slave_id=self.slave_id)
         if not result.isError():
             return result.bits
         else:
@@ -34,7 +27,6 @@ class Relay6CHTest:
         self.client.close()
 
 
-# --- Esempio d’uso ---
 if __name__ == '__main__':
     relay_tester = Relay6CHTest(port='/dev/ttyUSB0')  # Cambia la porta se necessario
     if relay_tester.connect():
